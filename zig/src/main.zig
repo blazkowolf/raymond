bdd73aa68f7a433cb4dc88c1dc30be8475c37d1e
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-04-04 00:30:47 -0400

    feat(zig): get textured walls working

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,40 +1,67 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-const world_map = [24][24]u32{
-    [_]u32{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-    [_]u32{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+const tg = @import("./texgen.zig");
+
+const screen_width = 640;
+const screen_height = 480;
+const tex_width = 64;
+const tex_height = 64;
+const map_width = 24;
+const map_height = 24;
+
+const world_map = [map_width][map_height]u32{
+    [_]u32{ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7 },
+    [_]u32{ 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 },
+    [_]u32{ 4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 },
+    [_]u32{ 4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7 },
+    [_]u32{ 4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7 },
+    [_]u32{ 4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1 },
+    [_]u32{ 4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8 },
+    [_]u32{ 4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1 },
+    [_]u32{ 4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1 },
+    [_]u32{ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+    [_]u32{ 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4 },
+    [_]u32{ 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+    [_]u32{ 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2 },
+    [_]u32{ 4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2 },
+    [_]u32{ 4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2 },
+    [_]u32{ 4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2 },
+    [_]u32{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2 },
+    [_]u32{ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 },
 };
 
 pub fn main() anyerror!void {
-    const screen_width = 800;
-    const screen_height = 450;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
 
-    var pos = rl.Vector2.init(22, 12);
-    var dir = rl.Vector2.init(-1, 0);
-    var plane = rl.Vector2.init(0, 0.66);
+    const allocator = arena.allocator();
+
+    var pos = rl.Vector2.init(22, 11.5); // (X,Y) start position
+    var dir = rl.Vector2.init(-1, 0); // Initial direction vector
+    var camera_plane = rl.Vector2.init(0, 0.66); // 2D raycaster camera plane
+    // var camera = rl.Camera2D{
+    //     .zoom = 1,
+    // };
+
+    // var buffer = rl.loadRenderTexture(screen_width, screen_height);
+    // defer buffer.unload();
+    var texture = [8]rl.Image{
+        tg.genImageDiagonalCross(allocator, tex_width, tex_height),
+        tg.genImageSlopedGreyscale(allocator, tex_width, tex_height),
+        tg.genImageSlopedYellowGradient(allocator, tex_width, tex_height),
+        tg.genImageXorGreyscale(allocator, tex_width, tex_height),
+        tg.genImageXorGreen(allocator, tex_width, tex_height),
+        tg.genImageRedBricks(allocator, tex_width, tex_height),
+        tg.genImageRedGradient(allocator, tex_width, tex_height),
+        tg.genImageFlatGrey(allocator, tex_width, tex_height),
+    };
 
     rl.initWindow(screen_width, screen_height, "raymond");
     defer rl.closeWindow(); // Close window and OpenGL context
@@ -45,11 +72,16 @@ pub fn main() anyerror!void {
         rl.beginDrawing();
         defer rl.endDrawing();
 
+        rl.clearBackground(rl.Color.black);
+
+        // camera.begin();
+        // defer camera.end();
+
         for (0..screen_width) |x| {
             const camera_x: f64 = 2 * @as(f64, @floatFromInt(x)) / @as(f64, @floatFromInt(screen_width)) - 1;
             const ray_dir = rl.Vector2.init(
-                dir.x + plane.x * @as(f32, @floatCast(camera_x)),
-                dir.y + plane.y * @as(f32, @floatCast(camera_x)),
+                dir.x + camera_plane.x * @as(f32, @floatCast(camera_x)),
+                dir.y + camera_plane.y * @as(f32, @floatCast(camera_x)),
             );
 
             var map_x = @as(i32, @intFromFloat(pos.x));
@@ -100,7 +132,7 @@ pub fn main() anyerror!void {
                 hit = world_map[@intCast(map_x)][@intCast(map_y)] > 0;
             }
 
-            const perp_wall_dist: f64 = if (side == 0) (side_dist.x - delta_dist.x) else (side_dist.y - delta_dist.y);
+            const perp_wall_dist: f32 = if (side == 0) (side_dist.x - delta_dist.x) else (side_dist.y - delta_dist.y);
 
             const line_height: i32 = @intFromFloat(@divFloor(
                 @as(f32, @floatFromInt(screen_height)),
@@ -117,24 +149,48 @@ pub fn main() anyerror!void {
                 draw_end = screen_height - 1;
             }
 
-            var color = switch (world_map[@intCast(map_x)][@intCast(map_y)]) {
-                1 => rl.Color.red,
-                2 => rl.Color.green,
-                3 => rl.Color.blue,
-                4 => rl.Color.white,
-                else => rl.Color.yellow,
-            };
+            // Texturing calculations
+            const tex_num = world_map[@intCast(map_x)][@intCast(map_y)] - 1;
 
-            if (side == 1) {
-                color = rl.colorBrightness(color, -0.5);
+            var wall_x: f32 = if (side == 0) pos.y + perp_wall_dist * ray_dir.y else pos.x + perp_wall_dist * ray_dir.x;
+            wall_x -= @floor(wall_x);
+
+            var tex_x: i32 = @intFromFloat(
+                wall_x * @as(f32, @floatFromInt(tex_width)),
+            );
+            if (side == 0 and ray_dir.x > 0) {
+                tex_x = tex_width - tex_x - 1;
+            }
+            if (side == 1 and ray_dir.y < 0) {
+                tex_x = tex_width - tex_x - 1;
             }
 
-            rl.drawLine(@intCast(x), draw_start, @intCast(x), draw_end, color);
+            const step: f32 = 1.0 * @as(
+                f32,
+                @floatFromInt(tex_height),
+            ) / @as(
+                f32,
+                @floatFromInt(line_height),
+            );
+
+            var tex_pos: f32 = (@as(f32, @floatFromInt(draw_start)) - @as(f32, @floatFromInt(@divFloor(screen_height, 2))) + @as(f32, @floatFromInt(@divFloor(line_height, 2)))) * step;
+
+            for (@intCast(draw_start)..@intCast(draw_end)) |y| {
+                const tex_y: i32 = @as(i32, @intFromFloat(tex_pos)) & (tex_height - 1);
+                tex_pos += step;
+                var color = texture[tex_num].getColor(tex_x, tex_y);
+                if (side == 1) {
+                    color = color.brightness(-0.5);
+                }
+                // rl.beginTextureMode(buffer);
+                rl.drawPixel(@intCast(x), @intCast(y), color);
+                // rl.endTextureMode();
+            }
         }
 
+        // rl.drawTexture(buffer.texture, 0, 0, rl.Color.white);
         rl.drawFPS(screen_width - 100, 50);
-
-        rl.clearBackground(rl.Color.black);
+        // rl.endDrawing();
 
         const frame_time = rl.getFrameTime();
         const move_speed: f32 = frame_time * 5;
@@ -162,18 +218,18 @@ pub fn main() anyerror!void {
             const old_dir_x = dir.x;
             dir.x = dir.x * @cos(-rot_speed) - dir.y * @sin(-rot_speed);
             dir.y = old_dir_x * @sin(-rot_speed) + dir.y * @cos(-rot_speed);
-            const old_plane_x = plane.x;
-            plane.x = plane.x * @cos(-rot_speed) - plane.y * @sin(-rot_speed);
-            plane.y = old_plane_x * @sin(-rot_speed) + plane.y * @cos(-rot_speed);
+            const old_plane_x = camera_plane.x;
+            camera_plane.x = camera_plane.x * @cos(-rot_speed) - camera_plane.y * @sin(-rot_speed);
+            camera_plane.y = old_plane_x * @sin(-rot_speed) + camera_plane.y * @cos(-rot_speed);
         }
 
         if (rl.isKeyDown(rl.KeyboardKey.key_a)) {
             const old_dir_x = dir.x;
             dir.x = dir.x * @cos(rot_speed) - dir.y * @sin(rot_speed);
             dir.y = old_dir_x * @sin(rot_speed) + dir.y * @cos(rot_speed);
-            const old_plane_x = plane.x;
-            plane.x = plane.x * @cos(rot_speed) - plane.y * @sin(rot_speed);
-            plane.y = old_plane_x * @sin(rot_speed) + plane.y * @cos(rot_speed);
+            const old_plane_x = camera_plane.x;
+            camera_plane.x = camera_plane.x * @cos(rot_speed) - camera_plane.y * @sin(rot_speed);
+            camera_plane.y = old_plane_x * @sin(rot_speed) + camera_plane.y * @cos(rot_speed);
         }
     }
 }
